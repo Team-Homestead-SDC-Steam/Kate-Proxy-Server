de20b084b8d32ef6054f806801fbdd855d6e4744
Christina Wang <cywang117@outlook.com>
2020-07-21 14:26:35 +0900

    Update AWS endpoints

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ gameReviews.get('/:gameid', (req, res) => {
   let { gameid } = req.params;
 
   // Build url string with filter query support
-  let urlStr = `http://ec2-3-14-148-108.us-east-2.compute.amazonaws.com:3001/api/gamereviews/${gameid}`;
+  let urlStr = `http://ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3001/api/gamereviews/${gameid}`;
   if (Object.keys(req.query).length) {
     urlStr += '?';
   }
@@ -44,7 +44,7 @@ const gameDesc = express.Router();
 app.use('/api/description', gameDesc);
 gameDesc.get('/:gameid', (req, res) => {
   let { gameid } = req.params;
-  fetch(`http://ec2-3-14-148-108.us-east-2.compute.amazonaws.com:3005/api/description/${gameid}`)
+  fetch(`http://ec2-13-59-202-34.us-east-2.compute.amazonaws.com:3005/api/description/${gameid}`)
     .then(response => response.json())
     .then(results => {
       res.status(200).json(results);
@@ -137,7 +137,7 @@ const dlc = express.Router();
 app.use('/api/dlc', dlc);
 dlc.get('/:gameid', (req, res) => {
   let { gameid } = req.params;
-  fetch(`http://localhost:3003/api/dlc/${gameid}`)
+  fetch(`http://ec2-54-193-86-65.us-west-1.compute.amazonaws.com:3003/api/dlc/${gameid}`)
     .then(response => response.json())
     .then(results => {
       res.status(200).json(results);
@@ -152,7 +152,7 @@ const gameName = express.Router();
 app.use('/api/name', gameName);
 gameName.get('/:gameid', (req, res) => {
   let { gameid } = req.params;
-  fetch(`http://localhost:3003/api/name/${gameid}`)
+  fetch(`http://ec2-54-193-86-65.us-west-1.compute.amazonaws.com:3003/api/name/${gameid}`)
     .then(response => response.json())
     .then(results => {
       res.status(200).json(results);
